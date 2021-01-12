8b43c5572faf15d6a005863237600f3c6760356c
Frank Buono <fbuonojr@fordham.edu>
2021-01-12 11:55:19 -0500

    fixing modal bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,22 +59,22 @@ $(document).ready(function () {
         });
     });
 
-    var modal = document.querySelector(".modal");
-    var trigger = document.querySelector(".trigger");
-    var closeButton = document.querySelector(".close-button");
+});
 
-    function toggleModal() {
-        modal.classList.toggle("show-modal");
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
     }
+}
 
-    function windowOnClick(event) {
-        if (event.target === modal) {
-            toggleModal();
-        }
-    }
-
-    trigger.addEventListener("click", toggleModal);
-    closeButton.addEventListener("click", toggleModal);
-    window.addEventListener("click", windowOnClick);
-
-})
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
